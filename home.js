cfe6c3240eed6331a9092c02fffbb5352b763ae3
dau minh tuan <dautuan032004@gmai.com>
2023-05-31 22:05:12 +0700

    mmessage

---
+++ home.js
@@ -0,0 +1,23 @@
+let first = document.querySelector('.first');
+function hello1(){
+    first.classList.add('hello1');
+    first.classList.remove('hello2');
+first.classList.remove('hello3');
+first.classList.remove('hello4');
+};
+function hello2(){
+    first.classList.add('hello2');
+    first.classList.remove('hello1');
+first.classList.remove('hello3');
+first.classList.remove('hello4');
+};function hello3(){
+    first.classList.add('hello3');
+    first.classList.remove('hello2');
+first.classList.remove('hello1');
+first.classList.remove('hello4');
+};function hello4(){
+    first.classList.add('hello4');
+    first.classList.remove('hello3');
+first.classList.remove('hello2');
+first.classList.remove('hello1');
+};
